d763fb018d44fc4244b78a6730b66aed0133fe86
Mgamboa <marianellag400@gmail.com>
2024-01-05 19:11:59 -0600

    layout for mobile and desktop finished. May tweak if needed. Javascript is the only thing left. Got inspo for base apparel challenge.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-// alert("hello world");//works
+// // alert("hello world");//works
 
-// all input
-let password = document.getElementById("exampleInputPassword1");
-// let password = document.myForm.firstname.value;
-let email = document.getElementById("exampleInputEmail1");
-let lastName = document.getElementById("exampleInputLastName");
-let firstName = document.getElementById("exampleInputFirstName");
+// // all input
+// let password = document.getElementById("exampleInputPassword1");
+// // let password = document.myForm.firstname.value;
+// let email = document.getElementById("exampleInputEmail1");
+// let lastName = document.getElementById("exampleInputLastName");
+// let firstName = document.getElementById("exampleInputFirstName");
 
 
-// https://www.javascripttutorial.net/javascript-dom/javascript-form-validation/
-// check this page out
+// // https://www.javascripttutorial.net/javascript-dom/javascript-form-validation/
+// // check this page out
