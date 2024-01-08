602411670479f10629a38996076354d7dfd7411c
Mgamboa <marianellag400@gmail.com>
2024-01-07 19:06:39 -0600

    tweaked and added stuff, error items are hidden, js will be worked on.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
 // console.log("hello world");//works
 const submit = document.querySelector(".submit");//button
 const input = document.querySelector("input");//hopefully all input
+const form = document.querySelector("form");
+// four error imgs
+const oneLabel = document.querySelector(".one");
+const twoLabel = document.querySelector(".two");
+const threeLabel = document.querySelector(".three");
+const fourLabel = document.querySelector(".four");
+
+form.addEventListener("submit", validate);
+submit.addEventListener("click", validate);
+
+function validate(e) {
+    e.preventDefault()//no re-load
+
+    const customerInfo = input.value;
+    input.value = ''
+    console.log(customerInfo);
+}
+
 
 // // all input
 // let password = document.getElementById("exampleInputPassword1");
